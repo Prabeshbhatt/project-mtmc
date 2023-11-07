38d914d450d6d968f88ebe226db786b2b0f98fce
Prabesh <Prabeshbhattarai82@gmail.com>
2023-11-07 12:12:04 +0530

    form one problem complete

--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,4 @@
 ﻿Public Class Form1
-    Private Sub RGPToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles RGPToolStripMenuItem.Click
-
-    End Sub
-
-
-    Private Sub RGPToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles RGPToolStripMenuItem1.Click
-        Dim SecondForm As New Outward
-
-        SecondForm.Show()
-    End Sub
-
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
@@ -17,22 +6,6 @@
 
     Private Sub Lgn_Click(sender As Object, e As EventArgs) Handles Lgn.Click
         Dim SecondForm As New Form4
-        SecondForm.Show()
-    End Sub
-
-    Private Sub OutwardToolStripMenuItem_Click(sender As Object, e As EventArgs)
-
-    End Sub
-
-    Private Sub INWARDToolStripMenuItem_Click(sender As Object, e As EventArgs)
-        Dim SecondForm As New Inward
-
-        SecondForm.Show()
-    End Sub
-
-    Private Sub RGPNRGPToolStripMenuItem_Click(sender As Object, e As EventArgs)
-        Dim SecondForm As New Outward
-
         SecondForm.Show()
     End Sub
 
