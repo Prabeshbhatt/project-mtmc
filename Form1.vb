865b58116a5830cb0734bb626214e22752df526b
SenpaiRrai <cajoprice@gmail.com>
2023-11-07 12:43:31 +0530

    form 1

--- Form1.vb
+++ Form1.vb
@@ -1,15 +1,18 @@
 ﻿Public Class Form1
 
+    Public Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+    End Sub
 
     Private Sub Lgn_Click(sender As Object, e As EventArgs) Handles Lgn.Click
         Dim SecondForm As New Form4
         SecondForm.Show()
     End Sub
 
+
+
     Private Sub INWARDToolStripMenuItem_Click_1(sender As Object, e As EventArgs) Handles INWARDToolStripMenuItem.Click
+
         Dim SecondForm As New Inward
 
         SecondForm.Show()
