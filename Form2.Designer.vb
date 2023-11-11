5634e1cfb09fb93f7a864fe51f1f6b9f9b3b3383
yashninja21 <yashrajsonar591@gmail.com>
2023-11-11 10:33:19 +0530

    courier

---
+++ Form2.Designer.vb
@@ -0,0 +1,259 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Courier
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        TextBox3 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        TextBox10 = New TextBox()
+        DateTimePicker1 = New DateTimePicker()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(119, 55)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(119, 33)
+        Label1.TabIndex = 0
+        Label1.Text = "Sl_no"
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(119, 88)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(119, 33)
+        Label2.TabIndex = 1
+        Label2.Text = "To"
+        ' 
+        ' Label3
+        ' 
+        Label3.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label3.Location = New Point(119, 121)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(119, 33)
+        Label3.TabIndex = 2
+        Label3.Text = "From"
+        ' 
+        ' Label4
+        ' 
+        Label4.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label4.Location = New Point(119, 153)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(119, 33)
+        Label4.TabIndex = 3
+        Label4.Text = "Date"
+        ' 
+        ' Label5
+        ' 
+        Label5.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label5.Location = New Point(119, 186)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(119, 33)
+        Label5.TabIndex = 4
+        Label5.Text = "Time"
+        ' 
+        ' Label6
+        ' 
+        Label6.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label6.Location = New Point(119, 220)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(119, 33)
+        Label6.TabIndex = 5
+        Label6.Text = "Recieved"
+        ' 
+        ' Label7
+        ' 
+        Label7.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label7.Location = New Point(119, 257)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(119, 33)
+        Label7.TabIndex = 6
+        Label7.Text = "Product No"
+        ' 
+        ' Label8
+        ' 
+        Label8.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label8.Location = New Point(119, 290)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(119, 33)
+        Label8.TabIndex = 7
+        Label8.Text = "Department"
+        ' 
+        ' Label9
+        ' 
+        Label9.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label9.Location = New Point(119, 323)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(119, 33)
+        Label9.TabIndex = 8
+        Label9.Text = "Signature"
+        ' 
+        ' Label10
+        ' 
+        Label10.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Label10.Location = New Point(119, 359)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(119, 33)
+        Label10.TabIndex = 9
+        Label10.Text = "Remark"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(244, 53)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(229, 31)
+        TextBox1.TabIndex = 10
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(244, 85)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(229, 31)
+        TextBox2.TabIndex = 11
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.Location = New Point(244, 119)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(229, 31)
+        TextBox3.TabIndex = 12
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.Location = New Point(244, 186)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(229, 31)
+        TextBox5.TabIndex = 14
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.Location = New Point(244, 218)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(229, 31)
+        TextBox6.TabIndex = 15
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.Location = New Point(244, 255)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(229, 31)
+        TextBox7.TabIndex = 16
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.Location = New Point(244, 293)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(229, 31)
+        TextBox8.TabIndex = 17
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.Location = New Point(244, 326)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(229, 31)
+        TextBox9.TabIndex = 18
+        ' 
+        ' TextBox10
+        ' 
+        TextBox10.Location = New Point(244, 362)
+        TextBox10.Name = "TextBox10"
+        TextBox10.Size = New Size(229, 31)
+        TextBox10.TabIndex = 19
+        ' 
+        ' DateTimePicker1
+        ' 
+        DateTimePicker1.Location = New Point(244, 153)
+        DateTimePicker1.Name = "DateTimePicker1"
+        DateTimePicker1.Size = New Size(229, 31)
+        DateTimePicker1.TabIndex = 20
+        ' 
+        ' Courier
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1010, 482)
+        Controls.Add(DateTimePicker1)
+        Controls.Add(TextBox10)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        MdiChildrenMinimizedAnchorBottom = False
+        Name = "Courier"
+        Text = "Courier"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents TextBox10 As TextBox
+    Friend WithEvents DateTimePicker1 As DateTimePicker
+End Class
