54b9c6c5d8c76e204a5b7baa768e54ac536edf2e
SenpaiRrai <cajoprice@gmail.com>
2023-11-21 11:32:49 +0530

    linklabel added login

--- Form4.vb
+++ Form4.vb
@@ -35,8 +35,12 @@ Public Class Form4
         End Try
     End Sub
 
-    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+    Private Sub RegisterLink_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles RegisterLink.LinkClicked
         Dim SecondForm As New Register
         SecondForm.Show()
+    End Sub
+
+    Private Sub RstPW_LinkClicked(sender As Object, e As LinkLabelLinkClickedEventArgs) Handles RstPW.LinkClicked
+
     End Sub
 End Class
