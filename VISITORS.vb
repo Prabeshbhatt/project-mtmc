8d9d9b2dfe1e8aa74e4c911392f3fc5a73c6ee1d
Prabesh <Prabeshbhattarai82@gmail.com>
2023-11-24 10:33:40 +0530

    from HOMEPAGE_FOR_SECURITY will be open first.

--- VISITORS.vb
+++ VISITORS.vb
@@ -122,7 +122,5 @@ Public Class VISITORS
 
     End Sub
 
-    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
 
-    End Sub
 End Class
