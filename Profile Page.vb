4d52a620381c6507ca86d769ab1d7a7c8a6110bc
SenpaiRrai <cajoprice@gmail.com>
2023-11-25 12:24:32 +0530

    anchored forms, sky blue

--- Profile Page.vb
+++ Profile Page.vb
@@ -63,4 +63,8 @@ Public Class ProfilePage
     Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles PictureBox1.Click
 
     End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
 End Class
