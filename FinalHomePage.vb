d31130fbc8f365300c19a6cc59dc4605badc921f
SenpaiRrai <cajoprice@gmail.com>
2023-12-13 23:41:09 +0530

    panel added, colour set

--- FinalHomePage.vb
+++ FinalHomePage.vb
@@ -52,4 +52,8 @@
         profilepage.Show()
         Me.Hide()
     End Sub
+
+    Private Sub Guna2Button1_Click(sender As Object, e As EventArgs) Handles Guna2Button1.Click
+
+    End Sub
 End Class
--- User_Control/Uc_courier.Designer.vb
+++ User_Control/Uc_courier.Designer.vb
@@ -22,6 +22,12 @@ Partial Class Uc_courier
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Guna2Panel1 = New Guna.UI2.WinForms.Guna2Panel()
+        Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
         Button5 = New Button()
         Button4 = New Button()
         TextBox7 = New TextBox()
@@ -45,7 +51,60 @@ Partial Class Uc_courier
         Label10 = New Label()
         TextBox2 = New TextBox()
         TextBox1 = New TextBox()
+        Guna2Panel1.SuspendLayout()
         SuspendLayout()
+        ' 
+        ' Guna2Panel1
+        ' 
+        Guna2Panel1.Anchor = AnchorStyles.Bottom Or AnchorStyles.Left Or AnchorStyles.Right
+        Guna2Panel1.BackColor = Color.GhostWhite
+        Guna2Panel1.Controls.Add(Guna2Button1)
+        Guna2Panel1.Controls.Add(Button5)
+        Guna2Panel1.Controls.Add(Button4)
+        Guna2Panel1.Controls.Add(TextBox7)
+        Guna2Panel1.Controls.Add(DateTimePicker1)
+        Guna2Panel1.Controls.Add(Label1)
+        Guna2Panel1.Controls.Add(Label2)
+        Guna2Panel1.Controls.Add(Button1)
+        Guna2Panel1.Controls.Add(Label3)
+        Guna2Panel1.Controls.Add(TextBox10)
+        Guna2Panel1.Controls.Add(Label4)
+        Guna2Panel1.Controls.Add(TextBox9)
+        Guna2Panel1.Controls.Add(Label5)
+        Guna2Panel1.Controls.Add(TextBox8)
+        Guna2Panel1.Controls.Add(Label6)
+        Guna2Panel1.Controls.Add(Label7)
+        Guna2Panel1.Controls.Add(TextBox6)
+        Guna2Panel1.Controls.Add(Label8)
+        Guna2Panel1.Controls.Add(TextBox5)
+        Guna2Panel1.Controls.Add(Label9)
+        Guna2Panel1.Controls.Add(TextBox3)
+        Guna2Panel1.Controls.Add(Label10)
+        Guna2Panel1.Controls.Add(TextBox2)
+        Guna2Panel1.Controls.Add(TextBox1)
+        Guna2Panel1.CustomizableEdges = CustomizableEdges3
+        Guna2Panel1.Location = New Point(117, 43)
+        Guna2Panel1.Name = "Guna2Panel1"
+        Guna2Panel1.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        Guna2Panel1.Size = New Size(960, 900)
+        Guna2Panel1.TabIndex = 0
+        ' 
+        ' Guna2Button1
+        ' 
+        Guna2Button1.CustomizableEdges = CustomizableEdges1
+        Guna2Button1.DisabledState.BorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.CustomBorderColor = Color.DarkGray
+        Guna2Button1.DisabledState.FillColor = Color.FromArgb(CByte(169), CByte(169), CByte(169))
+        Guna2Button1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
+        Guna2Button1.FillColor = Color.SlateBlue
+        Guna2Button1.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
+        Guna2Button1.ForeColor = Color.White
+        Guna2Button1.Location = New Point(183, 762)
+        Guna2Button1.Name = "Guna2Button1"
+        Guna2Button1.ShadowDecoration.CustomizableEdges = CustomizableEdges2
+        Guna2Button1.Size = New Size(140, 60)
+        Guna2Button1.TabIndex = 97
+        Guna2Button1.Text = "UPDATE"
         ' 
         ' Button5
         ' 
@@ -54,10 +113,10 @@ Partial Class Uc_courier
         Button5.FlatStyle = FlatStyle.Flat
         Button5.Font = New Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point)
         Button5.ForeColor = Color.White
-        Button5.Location = New Point(182, 609)
+        Button5.Location = New Point(183, 696)
         Button5.Name = "Button5"
         Button5.Size = New Size(140, 60)
-        Button5.TabIndex = 73
+        Button5.TabIndex = 96
         Button5.Text = "Update"
         Button5.UseVisualStyleBackColor = False
         ' 
@@ -68,10 +127,10 @@ Partial Class Uc_courier
         Button4.FlatStyle = FlatStyle.Flat
         Button4.Font = New Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point)
         Button4.ForeColor = Color.White
-        Button4.Location = New Point(527, 609)
+        Button4.Location = New Point(400, 696)
         Button4.Name = "Button4"
         Button4.Size = New Size(140, 60)
-        Button4.TabIndex = 72
+        Button4.TabIndex = 95
         Button4.Text = "Delete"
         Button4.UseVisualStyleBackColor = False
         ' 
@@ -79,38 +138,38 @@ Partial Class Uc_courier
         ' 
         TextBox7.Anchor = AnchorStyles.None
         TextBox7.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox7.Location = New Point(706, 193)
+        TextBox7.Location = New Point(514, 280)
         TextBox7.Name = "TextBox7"
         TextBox7.Size = New Size(392, 37)
-        TextBox7.TabIndex = 66
+        TextBox7.TabIndex = 89
         ' 
         ' DateTimePicker1
         ' 
         DateTimePicker1.Anchor = AnchorStyles.None
         DateTimePicker1.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        DateTimePicker1.Location = New Point(91, 412)
+        DateTimePicker1.Location = New Point(55, 499)
         DateTimePicker1.Name = "DateTimePicker1"
         DateTimePicker1.Size = New Size(392, 37)
-        DateTimePicker1.TabIndex = 71
+        DateTimePicker1.TabIndex = 94
         ' 
         ' Label1
         ' 
         Label1.Anchor = AnchorStyles.None
         Label1.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label1.Location = New Point(95, 58)
+        Label1.Location = New Point(59, 145)
         Label1.Name = "Label1"
         Label1.Size = New Size(100, 36)
-        Label1.TabIndex = 51
+        Label1.TabIndex = 74
         Label1.Text = "Sl no"
         ' 
         ' Label2
         ' 
         Label2.Anchor = AnchorStyles.None
         Label2.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label2.Location = New Point(91, 152)
+        Label2.Location = New Point(55, 239)
         Label2.Name = "Label2"
         Label2.Size = New Size(94, 38)
-        Label2.TabIndex = 52
+        Label2.TabIndex = 75
         Label2.Text = "To"
         ' 
         ' Button1
@@ -120,10 +179,10 @@ Partial Class Uc_courier
         Button1.FlatStyle = FlatStyle.Flat
         Button1.Font = New Font("Segoe UI", 14F, FontStyle.Bold, GraphicsUnit.Point)
         Button1.ForeColor = Color.White
-        Button1.Location = New Point(838, 609)
+        Button1.Location = New Point(621, 696)
         Button1.Name = "Button1"
         Button1.Size = New Size(140, 60)
-        Button1.TabIndex = 70
+        Button1.TabIndex = 93
         Button1.Text = "Save"
         Button1.UseVisualStyleBackColor = False
         ' 
@@ -131,188 +190,168 @@ Partial Class Uc_courier
         ' 
         Label3.Anchor = AnchorStyles.None
         Label3.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label3.Location = New Point(91, 257)
+        Label3.Location = New Point(55, 344)
         Label3.Name = "Label3"
         Label3.Size = New Size(94, 38)
-        Label3.TabIndex = 53
+        Label3.TabIndex = 76
         Label3.Text = "From"
         ' 
         ' TextBox10
         ' 
         TextBox10.Anchor = AnchorStyles.None
         TextBox10.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox10.Location = New Point(706, 517)
+        TextBox10.Location = New Point(514, 604)
         TextBox10.Name = "TextBox10"
         TextBox10.Size = New Size(392, 37)
-        TextBox10.TabIndex = 69
+        TextBox10.TabIndex = 92
         ' 
         ' Label4
         ' 
         Label4.Anchor = AnchorStyles.None
         Label4.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label4.Location = New Point(91, 371)
+        Label4.Location = New Point(55, 458)
         Label4.Name = "Label4"
         Label4.Size = New Size(94, 38)
-        Label4.TabIndex = 54
+        Label4.TabIndex = 77
         Label4.Text = "Date"
         ' 
         ' TextBox9
         ' 
         TextBox9.Anchor = AnchorStyles.None
         TextBox9.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox9.Location = New Point(704, 412)
+        TextBox9.Location = New Point(512, 499)
         TextBox9.Name = "TextBox9"
         TextBox9.Size = New Size(392, 37)
-        TextBox9.TabIndex = 68
+        TextBox9.TabIndex = 91
         ' 
         ' Label5
         ' 
         Label5.Anchor = AnchorStyles.None
         Label5.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label5.Location = New Point(91, 476)
+        Label5.Location = New Point(55, 563)
         Label5.Name = "Label5"
         Label5.Size = New Size(94, 38)
-        Label5.TabIndex = 55
+        Label5.TabIndex = 78
         Label5.Text = "Time"
         ' 
         ' TextBox8
         ' 
         TextBox8.Anchor = AnchorStyles.None
         TextBox8.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox8.Location = New Point(704, 298)
+        TextBox8.Location = New Point(512, 385)
         TextBox8.Name = "TextBox8"
         TextBox8.Size = New Size(392, 37)
-        TextBox8.TabIndex = 67
+        TextBox8.TabIndex = 90
         ' 
         ' Label6
         ' 
         Label6.Anchor = AnchorStyles.None
         Label6.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label6.Location = New Point(706, 58)
+        Label6.Location = New Point(514, 145)
         Label6.Name = "Label6"
         Label6.Size = New Size(132, 38)
-        Label6.TabIndex = 56
+        Label6.TabIndex = 79
         Label6.Text = "Recieved"
         ' 
         ' Label7
         ' 
         Label7.Anchor = AnchorStyles.None
         Label7.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label7.Location = New Point(704, 152)
+        Label7.Location = New Point(512, 239)
         Label7.Name = "Label7"
         Label7.Size = New Size(177, 38)
-        Label7.TabIndex = 57
+        Label7.TabIndex = 80
         Label7.Text = "Product No"
         ' 
         ' TextBox6
         ' 
         TextBox6.Anchor = AnchorStyles.None
         TextBox6.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox6.Location = New Point(706, 97)
+        TextBox6.Location = New Point(514, 184)
         TextBox6.Name = "TextBox6"
         TextBox6.Size = New Size(392, 37)
-        TextBox6.TabIndex = 65
+        TextBox6.TabIndex = 88
         ' 
         ' Label8
         ' 
         Label8.Anchor = AnchorStyles.None
         Label8.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label8.Location = New Point(704, 257)
+        Label8.Location = New Point(512, 344)
         Label8.Name = "Label8"
         Label8.Size = New Size(177, 38)
-        Label8.TabIndex = 58
+        Label8.TabIndex = 81
         Label8.Text = "Department"
         ' 
         ' TextBox5
         ' 
         TextBox5.Anchor = AnchorStyles.None
         TextBox5.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox5.Location = New Point(91, 517)
+        TextBox5.Location = New Point(55, 604)
         TextBox5.Name = "TextBox5"
         TextBox5.Size = New Size(392, 37)
-        TextBox5.TabIndex = 64
+        TextBox5.TabIndex = 87
         ' 
         ' Label9
         ' 
         Label9.Anchor = AnchorStyles.None
         Label9.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label9.Location = New Point(704, 371)
+        Label9.Location = New Point(512, 458)
         Label9.Name = "Label9"
         Label9.Size = New Size(157, 38)
-        Label9.TabIndex = 59
+        Label9.TabIndex = 82
         Label9.Text = "Signature"
         ' 
         ' TextBox3
         ' 
         TextBox3.Anchor = AnchorStyles.None
         TextBox3.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox3.Location = New Point(91, 298)
+        TextBox3.Location = New Point(55, 385)
         TextBox3.Name = "TextBox3"
         TextBox3.Size = New Size(392, 37)
-        TextBox3.TabIndex = 63
+        TextBox3.TabIndex = 86
         ' 
         ' Label10
         ' 
         Label10.Anchor = AnchorStyles.None
         Label10.Font = New Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point)
-        Label10.Location = New Point(706, 487)
+        Label10.Location = New Point(514, 574)
         Label10.Name = "Label10"
         Label10.Size = New Size(126, 38)
-        Label10.TabIndex = 60
+        Label10.TabIndex = 83
         Label10.Text = "Remark"
         ' 
         ' TextBox2
         ' 
         TextBox2.Anchor = AnchorStyles.None
         TextBox2.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox2.Location = New Point(91, 193)
+        TextBox2.Location = New Point(55, 280)
         TextBox2.Name = "TextBox2"
         TextBox2.Size = New Size(392, 37)
-        TextBox2.TabIndex = 62
+        TextBox2.TabIndex = 85
         ' 
         ' TextBox1
         ' 
         TextBox1.Anchor = AnchorStyles.None
         TextBox1.Font = New Font("Segoe UI", 11F, FontStyle.Bold, GraphicsUnit.Point)
-        TextBox1.Location = New Point(91, 97)
+        TextBox1.Location = New Point(55, 184)
         TextBox1.Name = "TextBox1"
         TextBox1.Size = New Size(392, 37)
-        TextBox1.TabIndex = 61
+        TextBox1.TabIndex = 84
         ' 
         ' Uc_courier
         ' 
         AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        Controls.Add(Button5)
-        Controls.Add(Button4)
-        Controls.Add(TextBox7)
-        Controls.Add(DateTimePicker1)
-        Controls.Add(Label1)
-        Controls.Add(Label2)
-        Controls.Add(Button1)
-        Controls.Add(Label3)
-        Controls.Add(TextBox10)
-        Controls.Add(Label4)
-        Controls.Add(TextBox9)
-        Controls.Add(Label5)
-        Controls.Add(TextBox8)
-        Controls.Add(Label6)
-        Controls.Add(Label7)
-        Controls.Add(TextBox6)
-        Controls.Add(Label8)
-        Controls.Add(TextBox5)
-        Controls.Add(Label9)
-        Controls.Add(TextBox3)
-        Controls.Add(Label10)
-        Controls.Add(TextBox2)
-        Controls.Add(TextBox1)
+        BackColor = Color.SlateBlue
+        Controls.Add(Guna2Panel1)
         Name = "Uc_courier"
-        Size = New Size(1169, 682)
+        Size = New Size(1169, 966)
+        Guna2Panel1.ResumeLayout(False)
+        Guna2Panel1.PerformLayout()
         ResumeLayout(False)
-        PerformLayout()
     End Sub
 
-    Friend WithEvents Button5 As Button
+    Friend WithEvents Guna2Panel1 As Guna.UI2.WinForms.Guna2Panel
     Friend WithEvents Button4 As Button
     Friend WithEvents TextBox7 As TextBox
     Friend WithEvents DateTimePicker1 As DateTimePicker
@@ -335,4 +374,6 @@ Partial Class Uc_courier
     Friend WithEvents Label10 As Label
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Button5 As Button
 End Class
