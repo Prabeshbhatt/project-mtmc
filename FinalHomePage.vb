ddf0e9b92a365b9172958f68a4bcaff78b627581
yashninja21 <yashrajsonar591@gmail.com>
2023-12-14 12:13:34 +0530

    ttt

--- FinalHomePage.vb
+++ FinalHomePage.vb
@@ -52,4 +52,9 @@
         profilepage.Show()
         Me.Hide()
     End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim uc As New Uc_home()
+        AddUserControl(uc)
+    End Sub
 End Class
--- User_Control/Uc_courier.vb
+++ User_Control/Uc_courier.vb
@@ -1,3 +1,5 @@
 ﻿Public Class Uc_courier
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
 
+    End Sub
 End Class
